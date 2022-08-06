a92ec2353b8d403d4e6a20bfd4fa7a6e3416c942
Alexey <neomorphizm@gmail.com>
2022-08-06 19:25:54 +0300

    debug

--- labs/lab06/startup-script.sh
+++ labs/lab06/startup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /usr/appinit
-./install_proxychains.sh
-./install_mongod.sh
-./install_ruby.sh
-./deploy.sh
+#cd /usr/appinit
+./usr/appinit/install_proxychains.sh
+./usr/appinit/install_mongod.sh
+./usr/appinit/install_ruby.sh
+./usr/appinit/deploy.sh
