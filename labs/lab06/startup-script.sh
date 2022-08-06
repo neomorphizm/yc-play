df435cf73142317d0b2db2ccf8415f0a2a9d0cec
Alexey <neomorphizm@gmail.com>
2022-08-06 18:14:50 +0300

    lab06 scripts adjustments

--- labs/lab06/startup-script.sh
+++ labs/lab06/startup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/home/alexey/yc-play/labs/install_proxychains.sh
-/home/alexey/yc-play/labs/install_git.sh
-/home/alexey/yc-play/labs/lab6/install_mongod.sh
-/home/alexey/yc-play/labs/lab6/install_ruby.sh
-/home/alexey/yc-play/labs/lab6/deploy.sh
+cd /run/appinit
+./install_proxychains.sh
+./install_mongod.sh
+./install_ruby.sh
+./deploy.sh
